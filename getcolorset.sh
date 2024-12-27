e716256844b5eca24217d036674a4ed844425072
root <root@pi-star>
2024-12-27 16:25:38 -0500

    update

--- getcolorset.sh
+++ getcolorset.sh
@@ -15,6 +15,6 @@ m2=$(sed -n '/^[ \t]*\[ColorSet'"$SetNum"'\]/,/\[/s/^[ \t]*UpperText[ \t]*=[ \t]
 m3=$(sed -n '/^[ \t]*\[ColorSet'"$SetNum"'\]/,/\[/s/^[ \t]*LowerBack[ \t]*=[ \t]*//p' /usr/local/etc/Nextion_Support/Colors.ini)
 m4=$(sed -n '/^[ \t]*\[ColorSet'"$SetNum"'\]/,/\[/s/^[ \t]*LowerText[ \t]*=[ \t]*//p' /usr/local/etc/Nextion_Support/Colors.ini)
 m5=$(sed -n '/^[ \t]*\[ColorSet'"$SetNum"'\]/,/\[/s/^[ \t]*Split[ \t]*=[ \t]*//p' /usr/local/etc/Nextion_Support/Colors.ini)
-#m5=$(sed -n '/^[ \t]*\[General\]/,/\[/s/^[ \t]*Split[ \t]*=[ \t]*//p' /usr/local/etc/Nextion_Support/Colors.ini)
+#m6=$(sed -n '/^[ \t]*\[General\]/,/\[/s/^[ \t]*Split[ \t]*=[ \t]*//p' /usr/local/etc/Nextion_Support/Colors.ini)
 mt="$SetNum|$m5|$m1|$m2|$m3|$m4"
 echo "$mt"
