a971e47141f5746f61bd5729579e2f4c35c63d56
root <root@pi-star>
2024-12-28 08:53:39 -0500

    update

---
+++ getlhd2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#################################################
+#  Get Last Heard Liost from MMDVMHost Log	#
+#						#
+#						#
+#  VE3RD 			2020-05-03	#
+#################################################
+set -o errexit
+set -o pipefail
+
+######################################
+#Start of Main Program
+######################################
+
+f1=$(ls -tr /var/log/pi-star/MMDVM* | tail -1)
+list1=$(sudo sed -n '/received network end of voice transmission from/p' $f1 | sed 's/,//g' | tail -1)
+call=$(echo "$list1" | cut -d " " -f14)
+dur=$(echo "$list1" | cut -d " " -f18)
+pl=$(echo "$list1" | cut -d " " -f20)
+echo "$call|$dur|$pl"
+
