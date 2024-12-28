9476c60f8283637df8b70e66557277670266a7ff
root <root@pi-star>
2024-12-28 12:13:02 -0500

    update

--- getlhd2.sh
+++ getlhd2.sh
@@ -14,8 +14,12 @@ set -o pipefail
 
 f1=$(ls -tr /var/log/pi-star/MMDVM* | tail -1)
 list1=$(sudo sed -n '/received network end of voice transmission from/p' $f1 | sed 's/,//g' | tail -1)
+
 call=$(echo "$list1" | cut -d " " -f14)
 dur=$(echo "$list1" | cut -d " " -f18)
 pl=$(echo "$list1" | cut -d " " -f20)
-echo "$call|$dur|$pl"
+tm1=$(echo "$list1" | cut -d " " -f3 | cut -d ":" -f1)
+tm2=$(echo "$list1" | cut -d " " -f3 | cut -d ":" -f2)
+
+echo "$call|$dur|$pl|$tm1:$tm2"
 
