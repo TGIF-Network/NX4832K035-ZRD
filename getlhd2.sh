b6a34eddc28a046524e0cfffee6cb2d31ac8bed3
root <root@pi-star>
2024-12-29 12:47:06 -0500

    update

--- getlhd2.sh
+++ getlhd2.sh
@@ -22,18 +22,20 @@ set -o pipefail
 #fi
 f1=$(ls -tr /var/log/pi-star/MMDVM* | tail -1)
 list1=$(sudo sed -n '/received network end of voice transmission from/p' $f1 | sed 's/,//g' | tail -1)
-
-call=$(echo "$list1" | cut -d " " -f14)
+#echo "$list1"
+tg=$(echo "$list1" | cut -d " " -f17)
 dur=$(echo "$list1" | cut -d " " -f18)
 pl=$(echo "$list1" | cut -d " " -f20)
-#tm=$(echo "$list1" | cut -d " " -f3 | cut -d "." -f1)
-tm=$(echo "$list1" | cut -d " " -f3 )
-#echo "$tm"
-ltz=$(date +%Z)
-ltimd=$(TZ=EST date --date='2024-12-29 15:14 UTC')
-tm1=$(echo "$ltimd" | cut -d " " -f4)
-tim2=$(echo "${tm1:0:5}")
-echo "$tim2"
+
+
+list1=$(echo "${list1:3:19}")
+# date -d "${a:0:-1} UTC"
+
+tm1=$(date -d "${list1:0:-1} UTC")
+
+tm3=$(echo "$tm1" | cut -d " " -f4)
+tm2=$(echo "${tm3:0:5}")
+echo "$tg|$dur|$pl|$tm2"
 
 #lt=$(date -d "$tm UTC")
 
